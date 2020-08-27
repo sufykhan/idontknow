224e20d67fbb0322b5203eba76217753fc08bb63
Almighty Stranger <sufyankhan9678@gmail.com>
2020-08-27 11:50:18 +0530

    netlify connection

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <h1>Hello</h1>
+        <h1>Hello Sufyan</h1>
         <a
           className="App-link"
           href="https://reactjs.org"
